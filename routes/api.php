1b3821011a4b8bf5245da56c0f182bd714e5cf2e
Carlos Ferreira <conta@especializati.com.br>
2021-12-08 15:41:46 -0300

    Create Route Todo

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\{
+    TodoController
+};
 use Illuminate\Support\Facades\Route;
+
+Route::apiResource('todos', TodoController::class);
 
 Route::get('/', function () {
     return response()->json([
