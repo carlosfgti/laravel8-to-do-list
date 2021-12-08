3a07e3510f7e8128946429d6e8a77ffa9f175310
Carlos Ferreira <conta@especializati.com.br>
2021-12-08 15:44:54 -0300

    Create Factory Todo

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->name(),
+            'description' => $this->faker->sentence(10),
+            'completed' => false,
+        ];
+    }
+}
