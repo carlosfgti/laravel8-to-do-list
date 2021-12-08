077c0a7674419fcd889889432652769c91a5b5f7
Carlos Ferreira <conta@especializati.com.br>
2021-12-08 15:40:23 -0300

    Define Resource API

---
+++ app/Http/Resources/TodoResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TodoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => strtolower($this->name),
+            'body' => $this->description,
+            'completed' => $this->completed ? 'S' : 'N',
+            'date' => Carbon::create($this->created_at)->format('Y-m-d'),
+        ];
+    }
+}
